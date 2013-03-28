d2ca6f5ba9d6051a9d28f4404c87cbb411759130
sowmyasandeep <sowmyas@bu.edu>
2013-03-27 21:14:19 -0400

    Register User Management Design_Basic HTML UI.

---
+++ RegUserMgt.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <!-- twitter bootstrap -->
+    <link rel="stylesheet" type="text/css" href="/cs673p1/bootstrap/css/bootstrap-responsive.min.css" />
+    <link rel="stylesheet" type="text/css" href="/cs673p1/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="/cs673p1/css/bootstrap.css" />
+
+    <script type="text/javascript" src="/cs673p1/bootstrap/js/bootstrap.min.js" ></script>
+
+    <script type="text/javascript" src="/cs673p1/assets/fc3bf33e/jquery.js"></script>
+<script type="text/javascript" src="/cs673p1/assets/fc3bf33e/jquery.ba-bbq.js"></script>
+<title>Home - House</title>
+    <style>
+    </style>
+	</head>
+
+<body>
+<header class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar-inner">
+        <div class="container-fluid">
+            <ul class="nav">
+                <li><a href="/cs673p1/index.php?r=site/index">HOME</a></li>
+				 <li><a href="/cs673p1/index.php?r=site/contact">Contact me</a></li>
+				 </ul>
+            <div class="pull-right">
+                <ul class="nav">
+				<li><a href="/cs673p1/index.php?r=site/logout">Logout</a></li>
+				<li></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+</header>
+<h1>Welcome User</h1>
+<div id="content">
+<p><center>
+<h1>Here it is!!</h1></center></p>
+<div class="form">
+<form id="login-form" action="/cs673p1/index.php?r=site/login" method="GET">
+<div class="row">
+<p><center><img src="Blank.Jpg" class="img-polaroid" width="200" height="200"></img></p>
+<p><button class="btn btn-mini" type="button">Edit</button></p>
+<p><button class="btn btn-mini" type="button">Delete</button></p>
+</center></p>
+
+</div>
+<div class="row">
+<p><center><img src="Blank.Jpg" class="img-polaroid" width="200" height="200"></img></p>
+<p><button class="btn btn-mini" type="button">Edit</button></p>
+<p><button class="btn btn-mini" type="button">Delete</button></center></p>
+</div>
