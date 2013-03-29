b01f1c8ffd57e9f12ad1e277a5d8559a78a606fb
wangalrebt <wangalrebt@gmail.com>
2013-03-29 14:38:51 -0400

    update yiic.php

--- protected/yiic.php
+++ protected/yiic.php
@@ -2,6 +2,6 @@
 
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/yii/framework/yiic.php';
-$config=dirname(__FILE__).'protected/config/console.php';
+$config=dirname(__FILE__).'/protected/config/console.php';
 
 require_once($yiic);
