b1f8751fb5ebb290a685c03f2520bca870a36cb1
sowmyasandeep <sowmyas@bu.edu>
2013-04-05 11:15:09 -0400

    View Page(User Management Page)-Updated.

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -13,13 +13,35 @@
 )); ?>
 <?php $houses=$model->house; ?>
 <div> <h1>Your House Details</h1></div>
-<?php $count=0; ?>
+<div class="span3">
+    <div>
+        <ul>
+            
+<div><h5><?php $tempcount = 0;
+foreach($houses as $house) :  
+$tempcount++; 
+endforeach;
+if($tempcount == 0)
+echo"You Have No Houses Yet! ";
+  ?></h5></div>
+  <div><h3><?php echo CHtml::link('Create House', array('house/create')); ?></h3></div>
+
+	
+	<?php $count=0;?>
+			
 <?php foreach($houses as $house) : ?>
     <div> <h4> House <?php echo ($count+1); ?> </h4></div>
     <div><?php echo $house->address_1; ?>
         <?php echo $house->address_2;?> </div>
     <div><?php echo $house->city; ?> <?php echo $house->state; ?>
         <?php echo $house->zip_code; ?></div>
+		<?php echo CHtml::link('Delete house', array('house/delete', 'id'=>$model->id),
+                    array(
+                        'submit'=>array('house/delete', 'id'=>$model->id),
+                        'class' => 'delete','confirm'=>'This will remove the image. Are you sure?'
+                    )) ; ?>
+		<div><?php echo CHtml::link('Update House', array('house/update')); ?></div>
+		
     <?php $count ++; ?>
 <?php  endforeach; ?>
 
