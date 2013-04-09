89786159daa8515593ec34d0ef2833ec5f87de33
wangalrebt <wangalrebt@gmail.com>
2013-04-09 14:05:19 -0400

    bug-fix: add close </div> tag
    There is no close div tag for user/view page.

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -1,17 +1,11 @@
-
-<<<<<<< HEAD
-
 <h1>Welcome <?php echo $model->name; ?></h1>
-=======
-<h1>View User #<?php echo $model->name; ?></h1>
->>>>>>> 7e91d5c7cf0ccf73e174c1bd3355fedf05aa7f99
+
 
 <?php $this->widget('zii.widgets.CDetailView', array(
     'data'=>$model,
     'attributes'=>array(
         'name',
         'email',
-        'id',
 
     ),
 )); ?>
@@ -20,35 +14,36 @@
 <div class="span3">
     <div>
         <ul>
-            
-<div><h5><?php $tempcount = 0;
-foreach($houses as $house) :  
-$tempcount++; 
-endforeach;
-if($tempcount == 0)
-echo"You Have No Houses Yet! ";
-  ?></h5></div>
-  <div><h3><?php echo CHtml::link('Create House', array('house/create')); ?></h3></div>
 
-	
-	<?php $count=0;?>
-			
-<?php foreach($houses as $house) : ?>
-    <div> <h4> House <?php echo ($count+1); ?> </h4></div>
-    <div><?php echo $house->address_1; ?>
-        <?php echo $house->address_2;?> </div>
-    <div><?php echo $house->city; ?> <?php echo $house->state; ?>
-        <?php echo $house->zip_code; ?></div>
-		<?php echo CHtml::link('Delete house', array('house/delete', 'id'=>$house->id),
+            <div><h5><?php $tempcount = 0;
+                    foreach($houses as $house) :
+                        $tempcount++;
+                    endforeach;
+                    if($tempcount == 0)
+                        echo"You Have No Houses Yet! ";
+                    ?></h5></div>
+            <div><h3><?php echo CHtml::link('Create House', array('house/create')); ?></h3></div>
+
+
+            <?php $count=0;?>
+
+            <?php foreach($houses as $house) : ?>
+                <div> <h4> House <?php echo ($count+1); ?> </h4></div>
+                <div><?php echo $house->address_1; ?>
+                    <?php echo $house->address_2;?> </div>
+                <div><?php echo $house->city; ?> <?php echo $house->state; ?>
+                    <?php echo $house->zip_code; ?></div>
+                <?php echo CHtml::link('Delete house', array('house/delete', 'id'=>$house->id),
                     array(
                         'submit'=>array('house/delete', 'id'=>$house->id),
                         'class' => 'delete','confirm'=>'This will remove the image. Are you sure?'
                     )) ; ?>
-		<div><?php echo CHtml::link('Update House', array('house/update', 'id'=>$house->id); ?></div>
-		
-		
-    <?php $count ++; ?>
-<?php  endforeach; ?>
+                <div><?php echo CHtml::link('Update House', array('house/update', 'id'=>$house->id)); ?></div>
 
+
+                <?php $count ++; ?>
+            <?php  endforeach; ?>
+    </div>
+</div>
 
 
