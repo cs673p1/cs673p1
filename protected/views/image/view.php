1e1ac73e97985bf791fb1e5816dfa751e5486961
dougfunny <mike.l.mcmahon@gmail.com>
2013-04-21 18:26:12 -0300

    Update view.php
    Add Link To update
    
      <?php $images=$model->id; ?>
    <li><?php echo CHtml::link('Update image', array('image/update', 'id'=>$model->id)); ?></li>

--- protected/views/image/view.php
+++ protected/views/image/view.php
@@ -1,4 +1,5 @@
 <?php echo CHtml::link('Back To House', array('house/view', 'id'=>$model->house->id)); ?><br>
 <img src="<?php echo $model->image_address; ?>" height="300" width="400">
 
-
+  <?php $images=$model->id; ?>
+<li><?php echo CHtml::link('Update image', array('image/update', 'id'=>$model->id)); ?></li>
