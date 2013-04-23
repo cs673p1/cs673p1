593e51b408396875060f7e6d4b7c4507e3cdc2b7
dougfunny <mike.l.mcmahon@gmail.com>
2013-04-23 16:08:48 -0300

    Update ImageController.php

--- protected/controllers/ImageController.php
+++ protected/controllers/ImageController.php
@@ -1,173 +1,225 @@
 <?php
+require_once dirname(__FILE__)."/../lib/main.php";
 
 class ImageController extends Controller
 {
-  /**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout='//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+	 public $imagenurl;
+    public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'actions'=>array('index','view'),
+                'users'=>array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions'=>array('create'),
+                'roles'=>array('authenticated'),
+            ),
+            array('allow', // allow authenticated user and himself to update/delete
+                'actions'=>array('update', 'delete', 'admin'),
+                'roles'=>array('authenticated')
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+		
+		
 		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+array('image', 'file', 'types'=>'jpg, gif, png'),
+);
+    }
+ 
+ 
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id)
+    {
+        $this->render('view',array(
+            'model'=>$this->loadModel($id),
+        ));
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new Image;
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate()
+    { 
+	
+        $model=new Image;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['Image']))
-		{
-			$model->attributes=$_POST['Image'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        if(isset($_POST['house_id']))
+        {
+		
+ 
+		 
+            $files = $_FILES["files"];
+            $files = is_array($files) ? $files : array($files);
+ 
+            foreach ($files["tmp_name"] as $index => $value) {
+                $result = \Cloudinary\Uploader::upload($value, array(
+                    "tags" => "backend_photo_album",
+                ));
+ 
+                unlink($value);
+                error_log("Upload result: " . \PhotoAlbum\ret_var_dump($result));
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+                foreach ( $result as $key => $image_address ) {
+                    if ($key = 'url') {
+                        $model->image_address = $image_address;
+                    }
+                }
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+                $model->house_id = $_POST['house_id'];
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Image']))
-		{
-			$model->attributes=$_POST['Image'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+                if($model->save())
+                    $this->redirect(array('view','id'=>$model->id));
+            }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+}
+        
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
+        $this->render('create',array(
+            'model'=>$model,
+        ));
+		
+    }
 
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id)
+    {
+        $model=$this->loadModel($id);
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('Image');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Image('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Image']))
-			$model->attributes=$_GET['Image'];
+        $seller = $model->house->seller;
+        $params = array('Seller'=>$seller);
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        if (!Yii::app()->user->checkAccess('updateHouse', $params) && !Yii::app()->user->checkAccess('admin')){
+            throw new CHttpException(403, 'You are not authorized to perform this action');
+        }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Image the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=Image::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+        if(isset($_POST['Image']))
+        {
+            $model->attributes=$_POST['Image'];
+            if($model->save())
+                $this->redirect(array('view','id'=>$model->id));
+        }
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param Image $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='image-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+        $this->render('update',array(
+            'model'=>$model,
+        ));
+    }
+
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id)
+    {
+        $model=$this->loadModel($id);
+
+        $seller = $model->house->seller;
+        $params = array('Seller'=>$seller);
+
+        if (!Yii::app()->user->checkAccess('updateHouse', $params) && !Yii::app()->user->checkAccess('admin')){
+            throw new CHttpException(403, 'You are not authorized to perform this action');
+        }
+        $this->loadModel($id)->delete();
+
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+       if(!isset($_GET['ajax']))
+           $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('house/view', 'id'=>$model->house->id));
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex()
+    {
+        $dataProvider=new CActiveDataProvider('Image');
+        $this->render('index',array(
+            'dataProvider'=>$dataProvider,
+        ));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin()
+    {
+        $model=new Image('search');
+        $model->unsetAttributes();  // clear any default values
+        if(isset($_GET['Image']))
+            $model->attributes=$_GET['Image'];
+
+        $this->render('admin',array(
+            'model'=>$model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return Image the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id)
+    {
+        $model=Image::model()->findByPk($id);
+        if($model===null)
+            throw new CHttpException(404,'The requested page does not exist.');
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param Image $model the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if(isset($_POST['ajax']) && $_POST['ajax']==='image-form')
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
 }
