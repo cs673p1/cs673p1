2d984a9e526513f27647f2b4a5f97008f4e18a6f
yasemina <yasemina@bu.edu>
2013-04-27 17:18:39 -0300

    Update _form.php

--- protected/views/house/_form.php
+++ protected/views/house/_form.php
@@ -8,20 +8,20 @@
 
 <?php echo $form->errorSummary($model); ?>
 
-<?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
+<?php echo $form->textFieldRow($model,'title',array('class'=>'span3','maxlength'=>255)); ?>
 
-<?php echo $form->textFieldRow($model,'address_1',array('class'=>'span5','maxlength'=>256)); ?>
+<?php echo $form->textFieldRow($model,'address_1',array('class'=>'span3','maxlength'=>256)); ?>
 
-<?php echo $form->textFieldRow($model,'address_2',array('class'=>'span5','maxlength'=>256)); ?>
+<?php echo $form->textFieldRow($model,'address_2',array('class'=>'span3','maxlength'=>256)); ?>
 
-<?php echo $form->textFieldRow($model,'city',array('class'=>'span5','maxlength'=>19)); ?>
+<?php echo $form->textFieldRow($model,'city',array('class'=>'span3','maxlength'=>19)); ?>
 
 <?php echo $form->dropDownListRow($model, 'state', States::getStates(array("US"), true),
     array('prompt' => 'Select State')); ?>
 
-<?php echo $form->textFieldRow($model,'zip_code',array('class'=>'span5')); ?>
+<?php echo $form->textFieldRow($model,'zip_code',array('class'=>'span4')); ?>
 
-<?php echo $form->textAreaRow($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
+<?php echo $form->textAreaRow($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span4')); ?>
 
 <?php echo $form->dropDownListRow($model,'number_of_floor', $model->getNumberOptions(0,6), array('prompt' => '----')); ?>
 
