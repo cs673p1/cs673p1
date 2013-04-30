78d74cb8d2ff58c94e98355fb9e98ecd2fcb8115
wangalrebt <wangalrebt@gmail.com>
2013-04-30 14:33:19 -0400

    add contact me

--- protected/views/house/view.php
+++ protected/views/house/view.php
@@ -1,17 +1,21 @@
 <div class="span3">
     <div class="sidebar-nav affix">
         <ul class="nav nav-list">
-            <li class="nav-header">House</li>
-            <li><?php echo CHtml::link('Post house', array('house/create')); ?></li>
-            <li><?php echo CHtml::link('Update house', array('house/update', 'id'=>$model->id)) ; ?></li>
-            <li><?php echo CHtml::link('Delete house', array('house/delete', 'id'=>$model->id),
-                    array(
-                        'submit'=>array('house/delete', 'id'=>$model->id),
-                        'class' => 'delete','confirm'=>'This will remove the House. Are you sure?'
-                    )) ; ?></li>
+            <li class="nav-header">Contact Me</li>
+            <li><a href="mailto:<?php echo $model->user->email; ?>">Email Me</a> </li>
+            <?php if($model->user->id==Yii::app()->user->getId()) : ?>
+                <li class="nav-header">House</li>
+                <li><?php echo CHtml::link('Post house', array('house/create')); ?></li>
+                <li><?php echo CHtml::link('Update house', array('house/update', 'id'=>$model->id)) ; ?></li>
+                <li><?php echo CHtml::link('Delete house', array('house/delete', 'id'=>$model->id),
+                        array(
+                            'submit'=>array('house/delete', 'id'=>$model->id),
+                            'class' => 'delete','confirm'=>'This will remove the House. Are you sure?'
+                        )) ; ?>
+                </li>
+            <?php endif; ?>
             <?php if(!Yii::app()->user->isGuest): ?>
                 <li class="nav-header">Profile</li>
-                <li><?php echo CHtml::link('House Management') ; ?></li>
                 <li><?php echo CHtml::link('Profile update', array('user/update', 'id'=>Yii::app()->user->getId())); ?></li>
             <?php endif; ?>
         </ul>
